461c561d913a4358a1934f06f294e22eebf058d3
muhdamrullah <muneorion@gmail.com>
2015-06-08 09:01:09 +0000

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,9 +1,22 @@
 import speech_recognition as sr
-r = sr.Recognizer()
-with sr.WavFile("test3.wav") as source:
-    audio = r.record(source)
+import subprocess
 
-try:
-    print("Transcription: " + r.recognize(audio))
-except LookupError:
-    print("Could not understand audio")
+def voice2Audio():
+    print("Recording speech 10secs")
+    subprocess.call(['arecord', '-f', 'cd', '-t', 'wav', '-d', '10', '-q', '-r', '16000', '-c', '1', 'audio.wav'])
+
+def audio2Text():
+    r = sr.Recognizer()
+    with sr.WavFile("audio.wav") as source:
+        audio = r.record(source)
+
+    try:
+	print("Transcribing...")
+        x = "Transcription: " + r.recognize(audio)
+        print x
+    except LookupError:
+        print("Could not understand audio")
+
+while True:
+    voice2Audio()
+    audio2Text()
